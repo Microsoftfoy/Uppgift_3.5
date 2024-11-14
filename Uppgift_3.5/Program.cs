fadd62dbde3d3f9bf8cbc66c2a168ff8ce78e364
ercr07001 <ercr07001@E-PW05CGR0>
2024-11-14 12:35:06 +0100

    Add project files.

---
+++ Uppgift_3.5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace Uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett tal");
+            int nr = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Skriv in ett tal till");
+            int nr1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Välj ett räknesätt (Skriv nummret)");
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtraktion");
+            Console.WriteLine("3. Multiplikation");
+            Console.WriteLine("4. Division");
+            int alt = int.Parse(Console.ReadLine());
+
+            if (alt == 1)
+            {
+                nr = nr + nr1;
+                Console.WriteLine("Detta är ditt tal:" + " " + nr);
+            }
+            else if (alt == 2)
+            {
+                nr = nr - nr1;
+                Console.WriteLine("Detta är ditt tal:" + " " + nr);
+            }
+            else if (alt == 3)
+            {
+                nr = nr * nr1;
+                Console.WriteLine("Detta är ditt tal:" + " " + nr);
+            }
+            else if (alt == 4)
+            {
+                nr = nr / nr1;
+                Console.WriteLine("Detta är ditt tal:" + " " + nr);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
